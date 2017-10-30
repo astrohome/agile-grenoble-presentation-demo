ed76ffc72aec890f7adf50459dd3e4e51468dd6e
Illia Izotov <i.izotov@criteo.com>
2017-10-30 17:56:35 +0100

    Initial commit

---
+++ tracker/src/main/java/com/criteo/demo/tracker/model/User.java
@@ -0,0 +1,13 @@
+package com.criteo.demo.tracker.model;
+
+public class User {
+    private int id;
+
+    public User(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
