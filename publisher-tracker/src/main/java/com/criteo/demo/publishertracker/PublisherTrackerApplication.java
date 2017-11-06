8765e9b3c0b5deb6e1a9327a4ba8b47fcba8c124
Illia Izotov <i.izotov@criteo.com>
2017-11-06 17:38:54 +0100

    Publisher tracker

---
+++ publisher-tracker/src/main/java/com/criteo/demo/publishertracker/PublisherTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.criteo.demo.publishertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublisherTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublisherTrackerApplication.class, args);
+	}
+}
