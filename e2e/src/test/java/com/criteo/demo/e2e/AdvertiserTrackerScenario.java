16033db75f5980de9604749b77ac84adaf1315a2
astrohome <izotov.ilya@gmail.com>
2017-11-21 16:56:04 +0100

    Fix e2e.

--- e2e/src/test/java/com/criteo/demo/e2e/AdvertiserTrackerScenario.java
+++ e2e/src/test/java/com/criteo/demo/e2e/AdvertiserTrackerScenario.java
@@ -2,14 +2,79 @@ package com.criteo.demo.e2e;
 
 
 import com.criteo.demo.common.utils.HttpUtils;
+import com.datastax.driver.core.Session;
 import cucumber.api.DataTable;
 import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.junit.runner.RunWith;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.java.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.core.CassandraTemplate;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(SpringRunner.class)
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
 public class AdvertiserTrackerScenario {
+
+    @Configuration
+    @EnableCassandraRepositories("com.criteo.demo.engine.dao")
+    @EnableKafka
+    static class ContextConfiguration extends AbstractCassandraConfiguration {
+
+        @Override
+        public String getKeyspaceName() {
+            return "stats";
+        }
+
+        @Override
+        protected String getContactPoints() {
+            return "cassandra";
+        }
+
+        @Bean
+        public ProducerFactory<String, String> producerFactory() {
+            return new DefaultKafkaProducerFactory<>(producerConfigs());
+        }
+
+        @Bean
+        public Map<String, Object> producerConfigs() {
+            Map<String, Object> props = new HashMap<>();
+            props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+            props.put(ProducerConfig.RETRIES_CONFIG, 0);
+            props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+            props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+            props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+            props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+            props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+            return props;
+        }
+
+        @Bean
+        public KafkaTemplate<String, String> kafkaTemplate() {
+            return new KafkaTemplate<>(producerFactory());
+        }
+
+        @Bean
+        public CassandraTemplate cassandraTemplate(Session session) {
+            return new CassandraTemplate(session);
+        }
+    }
 
     @Given("^A user id \"([^\"]*)\" visits the product id \"([^\"]*)\"$")
     public void a_user_id_visits_the_product_id(String userId, String productId) throws Throwable {
